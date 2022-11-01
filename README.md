d4fbe30696ac04f9e9724b88c299bf137349bb46
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-11-01 02:49:13 -0400

    DB connection ok

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ error on type:
 
 ### You are using require in a Node.js environment
 In this case, check your package.json file for an property called type. If that is set to module, ES6 modules will be enabled and you will run into the error mentioned above (specifically ReferenceError: require is not defined in ES module scope, you can use import instead). Simply remove that entry and you will be able to use require.
+
+* Install mongoDB 
+*npm install mongoose
+* we go to mlab.com for free database on mongoDB
+* after we have a DB
+*npm i dotenv
+* we can create a .env file
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
-
+const { default: mongoose } = require("mongoose");
+require('dotenv/config')
 const app = express();
+
+// Middleware
+app.use("/skills", () => {
+  console.log("this is the middleware running when we hit Skills");
+});
+
+// this the middleware for authentication
+// app.use(auth)
 
 // Routes
 app.get("/", (req, res) => {
   res.send("We are on Home");
 });
 
+// app.get("/skills", (req, res) => {
+//   res.send("We are on Skills");
+// });
+
+// Connect to mongoDB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  () => console.log("Connected to DB!")
+);
 // How do we start listening to the server
-app.listen(3000);
+app.listen(process.env.PORT);
